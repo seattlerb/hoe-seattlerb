e43529505837f0122f6106e9371d4889ad992914
Ryan Davis <ryand@zenspider.com>
2009-06-17 13:41:27 -0800

    Refactored email task with publish.rb changes [git-p4: depot-paths = "//src/hoe-seattlerb/dev/": change = 5060]

--- lib/hoe/email.rb
+++ lib/hoe/email.rb
@@ -20,39 +20,27 @@ module Hoe::Email
   def define_email_tasks
     require 'net/smtp'
     require 'time'
-    require 'yaml'
-    require 'socket'
+
+    # attach to announcements
+    task :announce => :send_email
 
     desc "Send an announcement email."
-    task "email:send" do
-      from_name, from_email = author.first, email.first
-      # TODO: sanity check that config is set up
-      # REFACTOR:
-      subject, title, body, urls = announcement
-      body = [title, urls, body, urls].join("\n\n")
-
-      msgstr = [
-                "From: #{from_name} <#{from_email}>",
-                "To: #{email_to.join(", ")}",
-                "Subject: [ANN] #{subject}",
-                "Date: #{Time.now.rfc2822}",
-                "",
-                "#{body}",
-                ].join("\n")
+    task :send_email do
+      email = generate_email :full
 
       with_config do |conf, _|
-        pass = conf["email"]["pass"]
-        user = conf["email"]["user"]
         host = conf["email"]["host"]
-        auth = conf["email"]["auth"]
         port = conf["email"]["port"]
+        user = conf["email"]["user"]
+        pass = conf["email"]["pass"]
+        auth = conf["email"]["auth"]
 
         start_args = [Socket.gethostname, user, pass, auth].compact
 
         smtp = Net::SMTP.new(host, port)
         smtp.set_debug_output $stderr if $DEBUG
         smtp.start(*start_args) do |smtp|
-          smtp.send_message msgstr, from_email, *email_to
+          smtp.send_message email, from_email, *email_to
         end
       end
     end
