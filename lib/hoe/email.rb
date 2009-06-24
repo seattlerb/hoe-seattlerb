d6515f0ad3840662edd4e794a131b265cc6d3f7c
Eric Hodel <drbrain@segment7.net>
2009-06-24 01:53:15 -0800

    + Add support for TLS servers like gmail via smtp_tls gem [git-p4: depot-paths = "//src/hoe-seattlerb/dev/": change = 5255]

--- lib/hoe/email.rb
+++ lib/hoe/email.rb
@@ -5,8 +5,9 @@ module Hoe::Email
     "user" => nil,
     "pass" => nil,
     "host" => nil,
-    "port" => 25,
+    "port" => "smtp",
     "auth" => nil,
+    "tls"  => nil,
   }
 
   attr_reader :email_to
@@ -19,7 +20,10 @@ module Hoe::Email
 
   def define_email_tasks
     require 'net/smtp'
-    require 'time'
+    begin
+      require 'smtp_tls'
+    rescue LoadError
+    end
 
     # attach to announcements
     task :announce => :send_email
@@ -34,11 +38,20 @@ module Hoe::Email
         user = conf["email"]["user"]
         pass = conf["email"]["pass"]
         auth = conf["email"]["auth"]
+        tls  = conf["email"]["tls"]
+
+        port = Socket.getservbyname port unless Integer === port
+
+        tls = port != 25 if tls.nil?
 
         start_args = [Socket.gethostname, user, pass, auth].compact
 
+        raise 'gem install smtp_tls' if tls and
+          not Net::SMTP.method_defined? :starttls
+
         smtp = Net::SMTP.new(host, port)
         smtp.set_debug_output $stderr if $DEBUG
+        smtp.enable_starttls if tls
         smtp.start(*start_args) do |server|
           server.send_message message, Array(email).first, *email_to
         end
