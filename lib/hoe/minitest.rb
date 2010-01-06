7cb824df661cc1b634b0b03d66055cbf55521e67
Ryan Davis <ryand@zenspider.com>
2010-01-06 14:29:03 -0800

    + Added minitest development dependency if activated [git-p4: depot-paths = "//src/hoe-seattlerb/dev/": change = 5594]

--- lib/hoe/minitest.rb
+++ lib/hoe/minitest.rb
@@ -1,4 +1,11 @@
 module Hoe::Minitest
+  def initialize_minitest
+    require 'minitest/unit'
+    version = MiniTest::Unit::VERSION
+
+    extra_dev_deps << ['minitest', ">= #{version}"]
+  end
+
   def define_minitest_tasks
     self.testlib = :minitest
   end
