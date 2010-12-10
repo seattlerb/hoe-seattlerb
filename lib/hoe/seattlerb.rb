33165a1c1b6babb1124b5406f90a00131f8c29e0
Ryan Davis <ryand@zenspider.com>
2010-12-10 14:15:50 -0800

    + Removed rubyforge plugin activation [git-p4: depot-paths = "//src/hoe-seattlerb/dev/": change = 6051]

--- lib/hoe/seattlerb.rb
+++ lib/hoe/seattlerb.rb
@@ -1,7 +1,7 @@
-Hoe.plugin :minitest, :perforce, :email, :rubyforge
+Hoe.plugin :minitest, :perforce, :email
 
 module Hoe::Seattlerb
-  VERSION = "1.2.3"
+  VERSION = "1.2.4"
 
   def define_seattlerb_tasks
     # nothing to do
