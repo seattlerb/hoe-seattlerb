f6e4907ade2caa49ce132b81608fe18254229865
Ryan Davis <ryand@zenspider.com>
2011-09-09 13:57:09 -0800

    + Removed rubyforge rdoc uploads. Only doing docs.seattlerb.org now. - Added rdoc 3.9 dep to ensure isolated projects are happy with release [git-p4: depot-paths = "//src/hoe-seattlerb/dev/": change = 6633]

--- lib/hoe/seattlerb.rb
+++ lib/hoe/seattlerb.rb
@@ -1,23 +1,16 @@
 Hoe.plugin :minitest, :perforce, :email
 
 module Hoe::Seattlerb
-  VERSION = "1.2.6"
+  VERSION = "1.2.7"
+
+  def initialize_seattlerb
+    dependency "rdoc", "~> 3.9", :developer
+  end
 
   def define_seattlerb_tasks
     if Hoe.plugins.include? :publish then
-      path   = File.expand_path("~/.rubyforge/user-config.yml")
-      config = YAML.load(File.read(path)) rescue nil
-      if config then
-        base = "/var/www/gforge-projects"
-        dir  = "#{base}/#{rubyforge_name}/#{remote_rdoc_dir}"
-
-        rdoc_locations << "#{config["username"]}@rubyforge.org:#{dir}"
-
-        base = "/data/www/docs.seattlerb.org"
-        rdoc_locations << "docs.seattlerb.org:#{base}/#{remote_rdoc_dir}"
-      else
-        warn "Couldn't read #{path}. Run `rubyforge setup`."
-      end
+      base = "/data/www/docs.seattlerb.org"
+      rdoc_locations << "docs.seattlerb.org:#{base}/#{remote_rdoc_dir}"
     end
   end
 end
