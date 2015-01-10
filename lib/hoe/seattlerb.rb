86a4749b0ce15c9e6c78134ebe54242c56eed69a
Ryan Davis <ryand@zenspider.com>
2015-01-09 16:10:25 -0800

    prepped for release [git-p4: depot-paths = "//src/hoe-seattlerb/dev/": change = 9915]

--- lib/hoe/seattlerb.rb
+++ lib/hoe/seattlerb.rb
@@ -7,7 +7,7 @@ class Hoe; end # :nodoc: stfu rdoc... *sigh*
 # pull in other default plugins.
 
 module Hoe::Seattlerb
-  VERSION = "1.3.3" # :nodoc:
+  VERSION = "1.3.4" # :nodoc:
 
   ##
   # Define seattlerb's rdoc location.
